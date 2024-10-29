fac2b274da5aad198d5dc9f1879b6057cf5d2d54
Whitelotus07 <josephkazumi7@gmail.com>
2024-10-29 06:00:06 +0100

    Update Loading.tsx

--- src/Loading.tsx
+++ src/Loading.tsx
@@ -9,7 +9,7 @@ const Loading: React.FC = () => {
       <div className="hexagon-wrapper">
         <Hexagon size={100} className="loading-hexagon spin-slow" />
       </div>
-      <p className="loading-text">Katoshi Labs</p>
+      <p className="loading-text text-white animate-pulse">Katoshi Labs</p>
     </div>
   );
 };
